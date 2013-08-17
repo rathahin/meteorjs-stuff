baa7e30828811406cb8840f98fec5cc89d627dbe
Roman Heinrich <roman.heinrich@gmail.com>
2013-08-17 20:06:57 +0200

    Squashed 'github.com/raix/Meteor-GroundDB/' content from commit 9158efe
    git-subtree-dir: github.com/raix/Meteor-GroundDB
    git-subtree-split: 9158efe38064167b10b606ae6e21daf96adde3c3

---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  summary: "\u001b[32mv0.0.16\n"+
+  "\u001b[33m-----------------------------------------\n"+
+  "\u001b[0m GroundDB is a thin layer providing       \n"+
+  "\u001b[0m Meteor offline database and methods      \n"+
+  "\u001b[33m-------------------------------------RaiX\n"
+});
+
+Package.on_use(function (api) {
+  "use strict";
+  api.export('GroundDB');
+  api.export('_gDB', ['client', 'server'], {testOnly: true});
+  api.use(['meteor', 'underscore', 'random', 'minimongo', 'ejson', 'ejson-minimax'],
+          ['client', 'server']);
+
+  api.use(['deps'], 'client');
+  //api.use([], 'server');
+  //api.use(['localstorage', 'ejson'], 'client');
+  api.add_files('groundDB.client.js', 'client');
+  api.add_files('groundDB.server.js', 'server');
+});
+
+Package.on_test(function (api) {
+  api.use('groundDB', ['client']);
+  api.use('test-helpers', 'client');
+  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
+           'random', 'deps']);
+
+  api.add_files('groundDB.client.tests.js', 'client');
+  api.add_files('groundDB.server.tests.js', 'server');
+});
